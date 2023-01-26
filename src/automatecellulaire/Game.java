9fa3d36e53bd87a50ab185cecf99215a134ee7c4
Alpha <1600972@gmail.com>
2023-01-26 15:33:56 +0100

    init de ma branch

---
+++ src/automatecellulaire/Game.java
@@ -0,0 +1,4 @@
+package automatecellulaire;
+public class Game{
+    
+}
---
+++ src/automatecellulaire/Grille.java
@@ -0,0 +1,13 @@
+package automatecellulaire;
+public class Grille{
+    private String [][] grille;
+    private Integer line,colum;
+    public Grille(Integer line , Integer colum){
+        this.grille=new String[line][colum];
+    }
+    public void initGrille(){
+        for (int i = 0; i < this.grille.length; i++){
+   
+        }
+    }
+}
