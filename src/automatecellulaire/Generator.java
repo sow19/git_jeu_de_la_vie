4e866a37797bce1a88608c5438de714b5d125492
Mariama Saoudatou Sow <sow224@c304l-d20p50.campus.unicaen.fr>
2023-01-30 13:20:42 +0100

    modification de nextGeneration

--- src/automatecellulaire/Generator.java
+++ src/automatecellulaire/Generator.java
@@ -20,24 +20,24 @@ public class Generator {
      * this function mimics the new generation
      * @param old_grid The grid
      */
-    public void nextGeneration(Grille old_grid){
-        // old_grid = new Grille(10, 10);
-        Grille future_grid = new Grille(null,null) ;
+    public void nextGeneration(Grid old_grid){
+        // old_grid = new Grid(10, 10);
+        Grid future_grid = new Grid(null,null) ;
 
         //walk through the grid and check the number of neighbors for each cell
 
-        for(int i =1;i+1< old_grid.getLine();i++){
-            for( int j =1;j+1< old_grid.getColum();j++){
+        for(int i =1;i+1< old_grid.getNbLine();i++){
+            for( int j =1;j+1< old_grid.getNbColum();j++){
 
-                int count = Collections.frequency(List.of(old_grid.getGrille()[i-1][j-1],old_grid.getGrille()[i-1][j]
-                ,old_grid.getGrille()[i-1][j+1],old_grid.getGrille()[i][j-1],old_grid.getGrille()[i][j+1]),true);
-                if(old_grid.getGrille()[i][j].getEtat() ==true && (count !=2 && count !=3)){
-                    future_grid.getGrille()[i][j].setEtat(false);
+                int count = Collections.frequency(List.of(old_grid.getGrid()[i-1][j-1],old_grid.getGrid()[i-1][j]
+                ,old_grid.getGrid()[i-1][j+1],old_grid.getGrid()[i][j-1],old_grid.getGrid()[i][j+1]),true);
+                if(old_grid.getGrid()[i][j].getEtat() ==true && (count !=2 && count !=3)){
+                    future_grid.getGrid()[i][j].setEtat(false);
 
-                }else if(old_grid.getGrille()[i][j].getEtat()==false && count==3){
-                    future_grid.getGrille()[i][j].setEtat(true);
+                }else if(old_grid.getGrid()[i][j].getEtat()==false && count==3){
+                    future_grid.getGrid()[i][j].setEtat(true);
                 }else{
-                    future_grid.getGrille()[i][j].setEtat(old_grid.getGrille()[i][j].getEtat())  ;
+                    future_grid.getGrid()[i][j].setEtat(old_grid.getGrid()[i][j].getEtat())  ;
                 }
                        
                 
