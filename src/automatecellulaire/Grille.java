65e40cf85971d2e78d328fa47f832894ff434b24
Alpha <1600972@gmail.com>
2023-01-30 08:54:36 +0100

    creation de la classe Grille

--- src/automatecellulaire/Grille.java
+++ src/automatecellulaire/Grille.java
@@ -1,13 +1,39 @@
 package automatecellulaire;
+
+import java.util.Random;
+
 public class Grille{
-    private String [][] grille;
-    private Integer line,colum;
+    private Cellule [][] board;
+    private Integer nbLine,nbColum;
     public Grille(Integer line , Integer colum){
-        this.grille=new String[line][colum];
+        this.board=new Cellule[line][colum];
+    }
+    public Integer getNbLine(){
+        return this.nbLine;
+    }
+    public Integer getNbColum(){
+        return this.nbColum;
     }
     public void initGrille(){
-        for (int i = 0; i < this.grille.length; i++){
-   
+        Random rd=new Random();
+        for (int i = 0; i < this.board.length; i++){
+            for (int j = 0; j < board[i].length; j++) {
+                this.board[i][j]=new Cellule(null, rd.nextBoolean());
+            }
         }
+    }
+    @Override
+    public String toString(){
+        String chaine=" -----"+System.lineSeparator();
+        for(int i=0;i<this.board.length;i++){
+            for(int j=0;j<this.board[i].length;j++){
+                chaine+="|"+this.board[i][j];
+            }
+            chaine+="|"+System.lineSeparator();
+            chaine+=" -----"+System.lineSeparator();
+        }
+        chaine+="\n";
+        // System.out.println("hashCode: "+this.hashCode());
+        return chaine;
     }
 }
