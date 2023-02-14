f3a81131bc6f625c5c497e389e361cc13595c45e
Marwa El Fejer <marwa.elfejer@etu.unicaen.fr>
2023-02-14 19:54:36 +0100

    initialiser la grill avec patterns et avec tableaux de position donner par l'utilisateur

--- src/automatecellulaire/Main.java
+++ src/automatecellulaire/Main.java
@@ -3,8 +3,9 @@ package automatecellulaire;
 public class Main {
 
 	public static void main(String[] args) {
-		Grid grid = new Grid(3, 3);
-		grid.initRandomGrid();
+		Grid grid = new Grid(10, 10);
+		//grid.initRandomGrid();
+		grid.initPattern("pattern.txt");
 		Generator generator = new Generator();
 		Game game = new Game(grid, generator);
 		
